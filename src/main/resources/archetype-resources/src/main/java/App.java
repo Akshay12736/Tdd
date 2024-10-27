d36b6aa5d557ecacf839259491ab5e7253e92322
Akshay12736 <akshayrupnar17@gmail.com>
2024-10-27 10:45:48 +0530

    new test

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.bdd.testproject.com.bdd.testproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/PomFramework/pomtestcases.java
@@ -0,0 +1,17 @@
+package PomFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class pomtestcases {
+
+	@Test
+	public void teston() {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("");
+
+	}
+}
